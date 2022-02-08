b4c387dddd9b983e1128eee88559e2cbb1cd7fa5
Ivan Dario Chacon Uribe <1022390613@u.icesi.edu.co>
2022-02-08 08:57:03 -0500

    JavaFX Example v1

---
+++ HelloWorldFX/src/application/SampleController.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class SampleController {
+	
+	@FXML
+	Label label1;
+	
+	@FXML
+	public void initialize() {
+		
+	}
+	
+}
