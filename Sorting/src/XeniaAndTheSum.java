49216801c69966b313566c1109534db9da5c3cd5
Ivan Dario Chacon Uribe <1022390613@u.icesi.edu.co>
2022-03-17 08:35:48 -0500

    Clase S8-M Sorting-Insertion

--- Sorting/src/XeniaAndTheSum.java
+++ Sorting/src/XeniaAndTheSum.java
@@ -2,8 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//Mala practica aprove (SOLO Competitiva) - Sin paquete
+
 public class XeniaAndTheSum {
 	
+	//Mala practica aprove (SOLO Competitiva) - Propagar excepciones
+	//Mala practica aprove (SOLO Competitiva) - codigo espagueti
 	public static void main(String[] args) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader in = new BufferedReader(isr);
---
+++ Sorting/src/sorting/InsertionSort.java
@@ -0,0 +1,19 @@
+package sorting;
+
+public class InsertionSort {
+
+	void insertionSort(int arr[], int n)
+	{
+	    for (int i = 1; i < n; i++) {
+	    	int j = i-1;
+	    	int val = arr[i];
+	    	while (j >= 0 && arr[j]> val) {
+	    		arr[j+1] = arr[j];
+	    		j--;
+	    	}
+	    	arr[j+1] = val;
+	    }
+	}
+	
+	
+}
